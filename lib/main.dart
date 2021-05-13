b32ee78d8bc553347f0347762faeeca1bac00613
Myllena Graciani <myllenagraciani@gmail.com>
2021-05-13 09:20:01 -0300

    retirado firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:aima/themes/light.theme.dart';
 import 'package:aima/ui/android/pages/login.page.dart';
-import 'package:aima/ui/android/pages/splash.screen.page.dart';
-import 'package:aima/ui/shared/pages/error.page.dart';
+
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() {
@@ -18,41 +17,12 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  /// The future is part of the state of our widget. We should not call `initializeApp`
-  /// directly inside [build].
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      // Initialize FlutterFire:
-      future: _initialization,
-      builder: (context, snapshot) {
-        // Check for errors
-        if (snapshot.hasError) {
-          return MaterialApp(
-            title: 'Aíma',
-            theme: lightTheme(),
-            home: ErrorPage(),
-          );
-        }
-
-        // Once complete, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-          return MaterialApp(
-            title: 'Aíma',
-            theme: lightTheme(),
-            home: LoginPage(),
-          );
-        }
-
-        // Otherwise, show something whilst waiting for initialization to complete
-        return MaterialApp(
-          title: 'Aíma',
-          theme: lightTheme(),
-          home: SplashScreen(),
-        );
-      },
+    return MaterialApp(
+      title: 'Aíma',
+      theme: lightTheme(),
+      home: LoginPage(),
     );
   }
 }
---
+++ lib/models/user.model.dart
@@ -0,0 +1,7 @@
+class UsuarioModel {
+  final String email;
+  final String senha;
+  // String nome;
+
+  UsuarioModel(this.email, this.senha);
+}
