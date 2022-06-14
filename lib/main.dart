f3839a3ee67d858c0c53d4800d8c522e9daef6b6
MyllaGraciani <myllenagraciani@gmail.com>
2022-06-14 01:23:28 -0300

    testando notificações

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,46 @@
 import 'package:aima/ui/pages/my_app.page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:workmanager/workmanager.dart';
+
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) {
+    FlutterLocalNotificationsPlugin flip =
+        new FlutterLocalNotificationsPlugin();
+    var android = new AndroidInitializationSettings('@mipmap/ic_launcher');
+    // ignore: non_constant_identifier_names
+    var IOS = new IOSInitializationSettings();
+    var settings = new InitializationSettings(android: android, iOS: IOS);
+    flip.initialize(settings);
+    _showNotificationWithDefaultSound(flip);
+    return Future.value(true);
+  });
+}
+
+Future _showNotificationWithDefaultSound(flip) async {
+  var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+    'your channel id',
+    'your channel name',
+    importance: Importance.max,
+    priority: Priority.high,
+  );
+  var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
+  var platformChannelSpecifics = new NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+      iOS: iOSPlatformChannelSpecifics);
+  await flip.show(0, 'Como você está se sentindo hoje?',
+      'Venha e registre o seu dia.', platformChannelSpecifics,
+      payload: 'Default_Sound');
+}
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  Workmanager().initialize(callbackDispatcher, isInDebugMode: false);
+  Workmanager().registerPeriodicTask(
+    "0",
+    "Como você está se sentindo hoje?",
+    frequency: Duration(minutes: 15),
+  );
   initializeDateFormatting().then((_) => runApp(MyApp()));
 }
